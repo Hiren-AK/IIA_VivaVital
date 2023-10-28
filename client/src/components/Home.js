d576510989b3f94c00c75ed148d8f144379cc17c
Hiren-AK <hirenarvind2013@gmail.com>
2023-10-29 02:47:52 +0530

    Initialized server and client

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to VivaVital</h1>
+      <p>Your health and nutrition tracker</p>
+    </div>
+  );
+}
+
+export default Home;
