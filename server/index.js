bfe28540ca3840baa819658b38a2a7a8025f1145
Hiren-AK <hirenarvind2013@gmail.com>
2023-10-29 20:42:12 +0530

    ETL for datasets added

--- server/index.js
+++ server/index.js
@@ -9,5 +9,5 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
