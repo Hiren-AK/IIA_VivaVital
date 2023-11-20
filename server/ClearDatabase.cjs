d5f750679ccc0d3aca96e0e3b850eab3865717db
Hiren-AK <hirenarvind2013@gmail.com>
2023-11-21 04:40:57 +0530

    Login and Registration both work, They give back the UserID in frontend

---
+++ server/ClearDatabase.cjs
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mysql = require('mysql2/promise'); // Use 'mysql2' for async/await support
+
+// Configuration for the MySQL connection
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'p@sswOrd123',
+  database: 'VivaVital',
+};
+
+// Function to delete the Initialized.flag file
+const deleteFlagFile = () => {
+  try {
+    fs.unlinkSync('./initialized.flag');
+    console.log('Initialized.flag deleted successfully.');
+  } catch (err) {
+    console.error('Error deleting Initialized.flag:', err.message);
+  }
+};
+
+// Function to execute SQL commands to drop tables
+const dropTables = async () => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    // Drop individual tables
+    await connection.query('DROP TABLE IF EXISTS Demographics, Dietary_Information, Dietary_Preferences, Health_Metrics, Nutritional_Information, Wearable_Data;');
+    await connection.query('DROP TABLE IF EXISTS Users;');
+
+    console.log('Tables dropped successfully.');
+
+    // Close the database connection
+    await connection.end();
+  } catch (err) {
+    console.error('Error dropping tables:', err.message);
+  }
+};
+
+// Main function to delete the flag file and drop tables
+const main = async () => {
+  // Delete the flag file
+  deleteFlagFile();
+
+  // Drop the tables
+  await dropTables();
+};
+
+// Run the main function
+main();
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,13 +11,12 @@ const validateEmail = (email) => {
 const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
 
-  // Basic Validation
   if (!username || !email || !password) {
     return res.status(400).send('Please provide username, email, and password');
   }
 
   if (!validateEmail(email)) {
-    return res.status(400).send('Invalid email format. Please provide a valid email address');
+    return res.status(400).send('Invalid email format');
   }
 
   if (password.length < 8) {
@@ -26,43 +25,25 @@ const registerUser = async (req, res) => {
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const query = 'INSERT INTO Users (Username, Email, PasswordHash) VALUES (?, ?, ?)';
-    
-    db.execute(query, [username, email, hashedPassword], (err, result) => {
-      if (err) {
-        if (err.code === 'ER_DUP_ENTRY') {
-          return res.status(409).send('Email already in use. Please choose another email');
-        }
-        console.error('Server error:', err.message); // Log detailed error message
-        res.status(500).send('Server error: ' + err.message); // Optionally send the error message in response for debugging
-      }
+    const insertQuery = 'INSERT INTO Users (Username, Email, PasswordHash) VALUES (?, ?, ?)';
+    await db.promise().query(insertQuery, [username, email, hashedPassword]);
+
+    const selectQuery = 'SELECT UserID FROM Users WHERE Email = ?';
+    const [users] = await db.promise().query(selectQuery, [email]);
+
+    if (users.length > 0) {
+      const userID = users[0].UserID;
+      res.status(201).json({ message: 'User registered successfully', userId: userID });
+    } else {
+      res.status(500).send('Error retrieving user ID');
     }
-    )} catch (err) {
-      console.error('Server error:', err.message); // Log detailed error message
-      res.status(500).send('Server error: ' + err.message); // Optionally send the error message in response for debugging
+  } catch (err) {
+    if (err.code === 'ER_DUP_ENTRY') {
+      res.status(409).send('Email already in use. Please choose another email');
+    } else {
+      console.error('Server error:', err.message);
+      res.status(500).send('Server error: ' + err.message);
     }
-    try{
-      const query2 = 'SELECT UserID, PasswordHash FROM Users WHERE Email = ?';
-      let user;
-      const [users] = await db.execute(query2, [email]);
-      if (users.length === 0) {
-        return res.status(401).send('Could not retreive UserID');
-      }
-      user = users[0];
-      try {
-        const isMatch = await bcrypt.compare(password, user.PasswordHash);
-        if (!isMatch) {
-          return res.status(401).send('Something went wrong in the backend');
-        }
-        res.status(200).send('User logged in successfully');
-      } catch (err) {
-        console.error('Server error:', err.message); // Log detailed error message
-        res.status(500).send('Server error: ' + err.message); // Optionally send the error message in response for debugging
-      }
-      res.status(201).json({ message: 'User registered successfully', userId: user.userId });
-    } catch (err) {
-    console.error('Server error:', err.message); // Log detailed error message
-    res.status(500).send('Server error: ' + err.message); // Optionally send the error message in response for debugging
   }
 };
 
@@ -73,28 +54,25 @@ const loginUser = async (req, res) => {
     return res.status(400).send('Please provide email and password');
   }
 
-  const query = 'SELECT UserID, PasswordHash FROM Users WHERE Email = ?';
-  
-  db.execute(query, [email], async (err, users) => {
-    if (err) {
-      return res.status(500).send('Error logging in');
-    }
-    if (users.length === 0) {
+  try {
+    const query = 'SELECT UserID, PasswordHash FROM Users WHERE Email = ?';
+    const [rows] = await db.promise().query(query, [email]);
+
+    if (rows.length === 0) {
       return res.status(401).send('Invalid email or password');
     }
 
-    try {
-      const user = users[0];
-      const isMatch = await bcrypt.compare(password, user.PasswordHash);
-      if (!isMatch) {
-        return res.status(401).send('Invalid email or password');
-      }
-      res.status(200).json({ message: 'User registered successfully', userId: user.userId });
-    } catch (err) {
-      console.error('Server error:', err.message); // Log detailed error message
-      res.status(500).send('Server error: ' + err.message); // Optionally send the error message in response for debugging
+    const user = rows[0];
+    const isMatch = await bcrypt.compare(password, user.PasswordHash);
+    if (!isMatch) {
+      return res.status(401).send('Invalid email or password');
     }
-  });
+
+    res.status(200).json({ message: 'User logged in successfully', userId: user.UserID });
+  } catch (err) {
+    console.error('Server error:', err.message);
+    res.status(500).send('Server error: ' + err.message);
+  }
 };
 
 const insertDemographics = async (req, res) => {
@@ -114,3 +92,30 @@ const insertDemographics = async (req, res) => {
 };
 
 export { registerUser, loginUser, insertDemographics };
+
+
+// function get_UserID(email, callback){
+      
+//   var sql = "SELECT UserID from Users where Email = ?";
+
+//   db.query(sql, function(err, results){
+//         if (err){ 
+//           throw err;
+//         }
+//         console.log(results[0].objid); // good
+//         stuff_i_want = results[0].objid;  // Scope is larger than function
+
+//         return callback(results[0].objid);
+// })
+// }
+
+
+// //usage
+
+// var stuff_i_want = '';
+
+// get_info(parm, function(result){
+// stuff_i_want = result;
+
+// //rest of your code goes in here
+// });
